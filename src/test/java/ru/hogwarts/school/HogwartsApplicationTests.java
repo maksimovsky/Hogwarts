4cea07c2c6e35139117928a1d8fcd98fc5073803
romax <maksimovskii.roman@yandex.ru>
2023-08-04 12:47:42 +0300

    1 commit

--- src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
+++ src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
@@ -1,13 +1,84 @@
 package ru.hogwarts.school;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+import ru.hogwarts.school.service.AvatarServiceImpl;
+import ru.hogwarts.school.service.FacultyServiceImpl;
+import ru.hogwarts.school.service.StudentServiceImpl;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
 class HogwartsApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @MockBean
+    private AvatarRepository avatarRepository;
+
+    @MockBean
+    private FacultyRepository facultyRepository;
+
+    @SpyBean
+    private StudentServiceImpl studentService;
+
+    @SpyBean
+    private AvatarServiceImpl avatarService;
+
+    @SpyBean
+    private FacultyServiceImpl facultyService;
+
+    @InjectMocks
+    private StudentController studentController;
+
     @Test
-    void contextLoads() {
+    public void getAllStudentsTest() {
+
     }
 
+    @Test
+    public void addStudentTest() throws Exception {
+        int id = 1;
+        String name = "student1";
+        int age = 15;
+
+        JSONObject studentObject = new JSONObject();
+        studentObject.put("name", name);
+        studentObject.put("age", age);
+
+        Student student = new Student(id, name, age);
+
+        when(studentRepository.save(any(Student.class))).thenReturn(student);
+        when(studentRepository.findById(any(Integer.class))).thenReturn(Optional.of(student));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/student")
+                        .content(studentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.age").value(age));
+    }
 }

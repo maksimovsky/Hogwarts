2a2756782b5870375d9c8402e2b62fa3349db328
romax <maksimovskii.roman@yandex.ru>
2023-08-22 23:01:55 +0300

    Homework done!

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,46 +13,56 @@ import java.util.Collection;
 public class FacultyServiceImpl implements FacultyService {
     private final FacultyRepository repository;
 
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
     public FacultyServiceImpl(FacultyRepository repository) {
         this.repository = repository;
     }
 
     public Collection<Faculty> getAll() {
+        logger.debug("Getting all faculties");
         return repository.findAll();
     }
 
     @Override
     public Faculty add(Faculty faculty) {
+        logger.debug("Adding faculty {}", faculty.getName());
         return repository.save(faculty);
     }
 
     @Override
     public Faculty getFacultyById(Integer id) {
+        logger.debug("Getting faculty by id {}", id);
         return repository.findById(id).orElse(null);
     }
 
     @Override
     public void removeFacultyById(Integer id) {
+        logger.debug("Removing faculty by id {}", id);
         repository.deleteById(id);
     }
 
     @Override
     public Faculty editFaculty(Faculty faculty) {
+        logger.debug("Editing faculty with id {}", faculty.getId());
         return repository.save(faculty);
     }
 
     @Override
     public Collection<Faculty> getSameColorFaculties(String color) {
+        logger.debug("Getting {} color faculties", color);
         return repository.findByColorIgnoreCase(color);
     }
 
     @Override
     public Collection<Faculty> getByNameOrColor(String s) {
+        logger.debug("Getting faculty by name or color: {}", s);
         return repository.findByNameIgnoreCaseContainsOrColorIgnoreCaseContains(s, s);
     }
 
     @Override
     public Collection<Student> getStudentsByFacultyId(int id) {
+        logger.debug("Getting students by faculty id {}", id);
         Faculty faculty = getFacultyById(id);
         if (faculty == null) {
             return null;
--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -12,61 +14,74 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository repository;
 
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     public StudentServiceImpl(StudentRepository repository) {
         this.repository = repository;
     }
 
     public Collection<Student> getAll() {
+        logger.debug("Getting all students");
         return repository.findAll();
     }
 
     @Override
     public Student add(Student student) {
+        logger.debug("Adding student {}", student.getName());
         return repository.save(student);
     }
 
     @Override
     public Student getStudentById(Integer id) {
+        logger.debug("Getting student by id {}", id);
         return repository.findById(id).orElse(null);
     }
 
     @Override
     public void removeStudentById(Integer id) {
+        logger.debug("Removing student by id {}", id);
         repository.deleteById(id);
     }
 
     @Override
     public Student editStudent(Student student) {
+        logger.debug("Editing student with id {}", student.getId());
         return repository.save(student);
     }
 
     @Override
     public Collection<Student> getSameAgeStudents(Integer age) {
+        logger.debug("Getting {} years old students", age);
         return repository.findByAge(age);
     }
 
     @Override
     public Collection<Student> findByAgeBetween(int from, int to) {
+        logger.debug("Getting students with age from {} to {}", from, to);
         return repository.findByAgeBetween(from, to);
     }
 
     @Override
     public Faculty getFacultyByStudentId(int id) {
+        logger.debug("Getting faculty by student id {}", id);
         return getStudentById(id).getFaculty();
     }
 
     @Override
     public int getCount() {
+        logger.debug("Getting count of students");
         return repository.getCount();
     }
 
     @Override
     public double getAverageAge() {
+        logger.debug("Getting average age of students");
         return repository.getAverageAge();
     }
 
     @Override
     public Collection<Student> getLast5Students() {
+        logger.debug("Getting last 5 students");
         return repository.getLast5Students();
     }
 }
